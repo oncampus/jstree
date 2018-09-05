d25bcacb548070a1be304401a3d38641f0fe1858
Dennis Krohn <dennis.krohn@fh-luebeck.de>
2018-09-05 13:32:48 +0200

    modified jstree.js for mediawiki compatibility

--- dist/jstree.js
+++ dist/jstree.js
@@ -1,15 +1,22 @@
 /*globals jQuery, define, module, exports, require, window, document, postMessage */
 (function (factory) {
 	"use strict";
+	/* adding this part for mediawiki compatibliity: */
+	$( document ).ready(function() {
+		factory(jQuery);
+	});
+	/* removing this part for mediawiki compatibility:
 	if (typeof define === 'function' && define.amd) {
 		define(['jquery'], factory);
 	}
+	
 	else if(typeof module !== 'undefined' && module.exports) {
 		module.exports = factory(require('jquery'));
 	}
 	else {
 		factory(jQuery);
 	}
+	*/
 }(function ($, undefined) {
 	"use strict";
 /*!
